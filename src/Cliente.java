112bebcdfb324d8c6d80964e2190542046ca8376
anlop31 <analopez31@correo.ugr.es>
2023-03-14 17:12:08 +0100

    primer commit

---
+++ src/Cliente.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+import java.util.Observer;
+import java.util.Observable;
+
+/**
+ *
+ * @author ana
+ */
+public class Cliente implements Observer {
+    @Override
+    public void update(Observable o, Object arg){
+        
+    }
+}

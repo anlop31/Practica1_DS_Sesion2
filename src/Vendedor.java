743e30c32481a57d5642f8c0181be6afc2ffdb8e
Ana López <77020767+anlop31@users.noreply.github.com>
2023-03-14 17:13:42 +0100

    Add files via upload

---
+++ src/Vendedor.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+import java.util.Observable;
+import java.util.Observer;
+
+/**
+ *
+ * @author ana
+ */
+public class Vendedor implements Observer {
+    @Override
+    public void update(Observable o, Object arg){
+        
+    }
+}
---
+++ src/Ventas.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+import java.util.Observable;
+import java.util.Observer;
+
+/**
+ *
+ * @author ana
+ */
+public class Ventas extends Observable{
+    // se genera una venta nueva --> notify
+    // almacenar las ventas
+}
